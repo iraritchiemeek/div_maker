edc29547742d246eb20807e38828f426178ca0a5
iraritchiemeek <iraritchiemeek@gmail.com>
2015-06-09 10:11:22 +1200

    Add shape

--- controller.js
+++ controller.js
@@ -2,19 +2,23 @@ $(document).ready(function() {
 
 	var shape = new Shape
 	var pointID = 0
+	var shapeID = 0
 
 	$(document).on("click", "#container", function(e) {
 		if ($(".point").length < 4) {
 			var x = e.pageX
 	      	var y = e.pageY
 			pointID += 1
-
 			shape.addPoint(pointID)
 			shape.positionPoint(x, y)
 		} else {
-			// shape.getPointsPos()
-			// shape.getPointsPos().then(console.log('yes'))
-			shape.generateSquare(shape.getPointsPos())
+			shapeID += 1
+			var positions = shape.getPointsPos()
+			var alignedPos = shape.alignSquare(positions)
+			var size = shape.getSize(alignedPos)
+			shape.generateSquare(shapeID)
+			shape.sizeSquare(size[0], size[1])
+			shape.positionShape(alignedPos)
 		}
 	})
 
--- shapes-model.js
+++ shapes-model.js
@@ -1,5 +1,11 @@
 function Shape() {
 	this.positions = []
+	this.width = 0
+	this.height = 0
+	this.hor = []
+	this.vert = []
+	this.top=[]
+	this.left=[]
 }
 
 Shape.prototype.addPoint = function(id) {
@@ -7,6 +13,7 @@ Shape.prototype.addPoint = function(id) {
 };
 
 Shape.prototype.positionPoint = function(x, y) {
+	console.log(x, y)
 	$(".point").last().css({top: y, left: x})
 };
 
@@ -18,6 +25,31 @@ Shape.prototype.getPointsPos = function() {
 	return(this.positions)
 };
 
-Shape.prototype.generateSquare = function(positions) {
-	console.log(positions)
+Shape.prototype.generateSquare = function(id) {
+	$("#container").append("<div class='shape' id='shape_" + id + "' />")
+};
+
+Shape.prototype.sizeSquare = function(width, height) {
+	$(".shape").last().css({width: (Math.abs(width)) + "px", height: (Math.abs(height)) + "px"})
+};
+
+Shape.prototype.getSize = function(positions) {
+	this.height = positions[2] - positions[0]
+	this.width = positions[3] - positions[1]
+	return [this.width,this.height]
+};
+
+Shape.prototype.alignSquare = function(positions) {
+	var that = this
+	$.each(positions, function(index, value) {
+		that.top.push(value["top"])
+		that.left.push(value["left"])
+	})
+	return [Math.min.apply(Math,this.top), Math.min.apply(Math, this.left), Math.max.apply(Math,this.top), Math.max.apply(Math,this.left)]
+};
+
+Shape.prototype.positionShape = function(positions) {
+	var top = (positions[0] + positions[2])/2
+	var left = (positions[1] + positions[3])/2
+	$(".shape").last().css({top: top, left: left})
 };
